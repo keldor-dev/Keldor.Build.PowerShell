148a25392f467dd22a41bfc0a3fad5de59c0877a
Skyler Hart <112896057+skyler-hart@users.noreply.github.com>
2026-07-07 10:44:54 -0400

    Add PowerShell module loader

---
+++ Keldor.Build.PowerShell.psm1
@@ -0,0 +1,24 @@
+$script:ModuleRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+foreach ($Path in @(
+    (Join-Path $script:ModuleRoot 'Private'),
+    (Join-Path $script:ModuleRoot 'Public')
+)) {
+    if (Test-Path -Path $Path) {
+        Get-ChildItem -Path $Path -Filter '*.ps1' -Recurse | Where-Object { -not $_.PSIsContainer } | ForEach-Object {
+            . $_.FullName
+        }
+    }
+}
+
+$PublicPath = Join-Path $script:ModuleRoot 'Public'
+
+if (Test-Path -Path $PublicPath) {
+    $PublicCommands = Get-ChildItem -Path $PublicPath -Filter '*.ps1' -Recurse |
+        Where-Object { -not $_.PSIsContainer } |
+        Select-Object -ExpandProperty BaseName
+
+    Export-ModuleMember -Function $PublicCommands
+}
+
+Register-KeldorPowerShellBuildProvider -ErrorAction SilentlyContinue | Out-Null
