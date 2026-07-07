c073df9774a91d3ce3586f53991f85378d7ecf26
Skyler Hart <112896057+skyler-hart@users.noreply.github.com>
2026-07-07 10:58:01 -0400

    Expand module tests

--- Tests/Keldor.Build.PowerShell.Tests.ps1
+++ Tests/Keldor.Build.PowerShell.Tests.ps1
@@ -1,25 +1,29 @@
 BeforeAll {
-    Import-Module (Join-Path $PSScriptRoot '..' 'Keldor.Build.PowerShell.psd1') -Force
+    Import-Module (Join-Path -Path $PSScriptRoot -ChildPath '..' | Join-Path -ChildPath 'Keldor.Build.PowerShell.psd1') -Force
 }
 
 Describe 'Keldor.Build.PowerShell' {
     It 'imports successfully' {
-        Get-Module Keldor.Build.PowerShell | Should -Not -BeNullOrEmpty
+        Get-Module -Name Keldor.Build.PowerShell | Should -Not -BeNullOrEmpty
     }
 
     It 'exports expected commands' {
         $Commands = Get-Command -Module Keldor.Build.PowerShell | Select-Object -ExpandProperty Name
 
+        $Commands | Should -Contain 'Clear-KeldorPowerShellBuild'
+        $Commands | Should -Contain 'Invoke-KeldorPowerShellBuild'
+        $Commands | Should -Contain 'Invoke-KeldorPowerShellProjectTests'
+        $Commands | Should -Contain 'Publish-KeldorPowerShellProject'
         $Commands | Should -Contain 'Register-KeldorPowerShellBuildProvider'
         $Commands | Should -Contain 'Resolve-KeldorPowerShellProject'
         $Commands | Should -Contain 'Test-KeldorPowerShellProject'
-        $Commands | Should -Contain 'Invoke-KeldorPowerShellBuild'
     }
 
-    It 'resolves this repository as a PowerShell project' {
-        $Project = Resolve-KeldorPowerShellProject -Path (Join-Path $PSScriptRoot '..')
+    It 'resolves this repository as a PowerShell module project' {
+        $Project = Resolve-KeldorPowerShellProject -Path (Join-Path -Path $PSScriptRoot -ChildPath '..')
 
         $Project.Name | Should -Be 'Keldor.Build.PowerShell'
         $Project.ProjectType | Should -Be 'PowerShellModule'
+        $Project.ManifestPath | Should -Match 'Keldor.Build.PowerShell.psd1'
     }
 }
